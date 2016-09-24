4ad48371523859d35e58905de5c5546b474349e4
mak- <mak@redbrick.dcu.ie>
2016-09-24 01:22:03 -0400

    ignore GET or POST yips

--- parameth.py
+++ parameth.py
@@ -8,24 +8,24 @@ import string
 from numpy import array_split
 
 requests.packages.urllib3.disable_warnings()
-BASE_GETstatus = 0
-BASE_GETresponseSize = 0
-BASE_POSTstatus = 0
-BASE_POSTresponseSize = 0
-BASE_POSTdata = {}
-BASE_paramValue = 'discobiscuits'
+_GETstatus = 0
+_GETresponseSize = 0
+_POSTstatus = 0
+_POSTresponseSize = 0
+_POSTdata = {}
+_paramValue = 'discobiscuits'
 
 def version_info():
-	VERSION_INFO = 'parameth v1.0 - \033[033mfind parameters and craic rocks\033[0m'
-	AUTHOR_INFO = 'Author: \033[033mCiaran McNally - https://makthepla.net\033[0m'
+	VER = 'parameth v1.0 - \033[033mfind parameters and craic rocks\033[0m'
+	AUTH = 'Author: \033[033mCiaran McNally - https://makthepla.net\033[0m'
 	print '\033[1;34m                                       |   |'
 	print '  __ \\   _` |  __| _` | __ `__ \\   _ \\ __| __ \\'
 	print '  |   | (   | |   (   | |   |   |  __/ |   | | |'
 	print '  .__/ \\__,_|_|  \\__,_|_|  _|  _|\\___|\\__|_| |_|'
 	print '  _|\033[0m'
-	print VERSION_INFO
-	print AUTHOR_INFO
-	print '\033[1;30m=====================================================\033[0m'
+	print VER
+	print AUTH
+	print '\033[1;30m================================================\033[0m'
 
 def getHeaderObj(header):
 	h1 = string.split(header, ':')[0]
@@ -76,7 +76,7 @@ def printOut(filename, string):
 	f.close()
 
 def requestor(url, parameter, header, agent, variance, proxy, 
-				ignore, data, out, size):
+				ignore, data, out, size, igmeth):
 	headers = {}
 	post = {}
 	proxies = {}
@@ -92,56 +92,58 @@ def requestor(url, parameter, header, agent, variance, proxy,
 		newrl = url
 		strvar = ''
 		post = {}
-		post[i] = BASE_paramValue
+		post[i] = _paramValue
 		if '?' in url:
-			newrl += '&' + i + '=' + BASE_paramValue
+			newrl += '&' + i + '=' + _paramValue
 		else:
-			newrl += '?' + i + '=' + BASE_paramValue
-		post.update(BASE_POSTdata)
+			newrl += '?' + i + '=' + _paramValue
+		post.update(_POSTdata)
 		try:	 
 			#GET parameter
-			g = requests.get(newrl, timeout=10, headers=header, 
-				allow_redirects=False, verify=False, proxies=proxies)
-			plusvar = len(g.content) + variance
-			subvar = len(g.content) - variance
+			if igmeth != 'g':
+				g = requests.get(newrl, timeout=10, headers=header, 
+					allow_redirects=False, verify=False, proxies=proxies)
+				plusvar = len(g.content) + variance
+				subvar = len(g.content) - variance
 
-			if g.status_code != BASE_GETstatus and statusMatch(ignore, str(g.status_code)):
-				print '\033[032mGET(status)\033[0m: '+i+' | '+str(BASE_GETstatus)+'->',
-				print str(g.status_code)+' ( '+newrl+' )'
-				if out != 'out':
-					strvar = 'GET(status) '+i+' '+str(g.status_code)+' '+newrl
-					printOut(out, strvar)
+				if g.status_code != _GETstatus and statusMatch(ignore, str(g.status_code)):
+					print '\033[032mGET(status)\033[0m: '+i+' | '+str(_GETstatus)+'->',
+					print str(g.status_code)+' ( '+newrl+' )'
+					if out != 'out':
+						strvar = 'GET(status) '+i+' '+str(g.status_code)+' '+newrl
+						printOut(out, strvar)
 			
-			if statusMatch(ignore, str(g.status_code)):
-				if len(g.content) != BASE_GETresponseSize and len(g.content) != size:
-					if len(g.content) >= plusvar or len(g.content) <= subvar:
-						print '\033[032mGET(size)\033[0m: '+i+' | '+str(BASE_GETresponseSize),
-						print '->' +str(len(g.content))+ ' ( '+newrl+' )'
-						if out != 'out':
-							strvar = 'GET(size) '+i+' '+str(len(g.content))+' '+newrl
-							printOut(out, strvar)
+				if statusMatch(ignore, str(g.status_code)):
+					if len(g.content) != _GETresponseSize and len(g.content) != size:
+						if len(g.content) >= plusvar or len(g.content) <= subvar:
+							print '\033[032mGET(size)\033[0m: '+i+' | '+str(_GETresponseSize),
+							print '->' +str(len(g.content))+ ' ( '+newrl+' )'
+							if out != 'out':
+								strvar = 'GET(size) '+i+' '+str(len(g.content))+' '+newrl
+								printOut(out, strvar)
 			
 			#POST parameter
-			p = requests.post(url, timeout=10, headers=header, data=post,
-				allow_redirects=False, verify=False, proxies=proxies)
-			plusvar = len(p.content) + variance
-			subvar = len(p.content) - variance
+			if igmeth != 'p':
+				p = requests.post(url, timeout=10, headers=header, data=post,
+					allow_redirects=False, verify=False, proxies=proxies)
+				plusvar = len(p.content) + variance
+				subvar = len(p.content) - variance
 
-			if p.status_code != BASE_POSTstatus and statusMatch(ignore, str(p.status_code)):
-				print '\033[032mPOST(status)\033[0m: '+i+' | '+str(BASE_POSTstatus)+'->',
-				print str(p.status_code)+' ( '+url+' )'
-				if out != 'out':
-					strvar = 'POST(status) '+i+' '+str(p.status_code)+' '+url
-					printOut(out, strvar)
+				if p.status_code != _POSTstatus and statusMatch(ignore, str(p.status_code)):
+					print '\033[032mPOST(status)\033[0m: '+i+' | '+str(_POSTstatus)+'->',
+					print str(p.status_code)+' ( '+url+' )'
+					if out != 'out':
+						strvar = 'POST(status) '+i+' '+str(p.status_code)+' '+url
+						printOut(out, strvar)
 			
-			if statusMatch(ignore, str(p.status_code)):
-				if len(p.content) != BASE_POSTresponseSize and len(p.content) != size:
-					if len(p.content) >= plusvar or len(p.content) <= subvar:
-						print '\033[032mPOST(size)\033[0m: '+i+' | '+str(BASE_POSTresponseSize),
-						print '->' +str(len(p.content))+ ' ( '+url+' )'
-						if out != 'out':
-							strvar = 'POST(size) '+i+' '+str(len(p.content))+' '+url
-							printOut(out, strvar)
+				if statusMatch(ignore, str(p.status_code)):
+					if len(p.content) != _POSTresponseSize and len(p.content) != size:
+						if len(p.content) >= plusvar or len(p.content) <= subvar:
+							print '\033[032mPOST(size)\033[0m: '+i+' | '+str(_POSTresponseSize),
+							print '->' +str(len(p.content))+ ' ( '+url+' )'
+							if out != 'out':
+								strvar = 'POST(size) '+i+' '+str(len(p.content))+' '+url
+								printOut(out, strvar)
 
 		except requests.exceptions.Timeout:
 			print 'Request Timed out on parameter "'+i+'"'
@@ -151,16 +153,16 @@ def requestor(url, parameter, header, agent, variance, proxy,
 			print 'Redirect loop on parameter "'+i+'"'		
 	
 
-def getBase(url, header, agent, variance, proxy, data):
+def getBase(url, header, agent, variance, proxy, data, igmeth):
 	headers = {}
 	proxies = {}
 	get = ''
 	url_base = ''
-	global BASE_GETstatus
-	global BASE_GETresponseSize	
-	global BASE_POSTstatus
-	global BASE_POSTresponseSize
-	global BASE_POSTdata
+	global _GETstatus
+	global _GETresponseSize	
+	global _POSTstatus
+	global _POSTresponseSize
+	global _POSTdata
 	
 	if ':' in header:
 		headers = getHeaderObj(header)
@@ -173,28 +175,32 @@ def getBase(url, header, agent, variance, proxy, data):
 	else:
 		url_base = url
 	
-	BASE_POSTdata = getParamObj(get)
+	_POSTdata = getParamObj(get)
 	if len(data) != 0:
-		BASE_POSTdata.update(getParamObj(data))
+		_POSTdata.update(getParamObj(data))
 	print 'Establishing base figures...'
-	print '\033[031mPOST data: \033[0m'+getParamStr(BASE_POSTdata)
+	if igmeth != 'p':
+		print '\033[031mPOST data: \033[0m'+getParamStr(_POSTdata)
 	print '\033[031mOffset value: \033[0m'+str(variance)
 	try:
-		g = requests.get(url, timeout=10, headers=headers, verify=False,
-							allow_redirects=False, proxies=proxies)
-		BASE_GETstatus = g.status_code
-		BASE_GETresponseSize = len(g.content)
-		print '\033[031mGET: content-length->\033[0m '+str(len(g.content)),
-		print '\033[031m status->\033[0m '+str(g.status_code)
+		if igmeth != 'g':
+			g = requests.get(url, timeout=10, headers=headers, verify=False,
+								allow_redirects=False, proxies=proxies)
+			_GETstatus = g.status_code
+			_GETresponseSize = len(g.content)
+			print '\033[031mGET: content-length->\033[0m '+str(len(g.content)),
+			print '\033[031m status->\033[0m '+str(g.status_code)
 	
-		p = requests.post(url_base, timeout=10, headers=headers, verify=False,
-							allow_redirects=False, proxies=proxies, data=BASE_POSTdata)
-		BASE_POSTstatus = p.status_code
-		BASE_POSTresponseSize = len(p.content)
-		print '\033[031mPOST: content-length->\033[0m '+str(len(p.content)),
-		print '\033[031m status->\033[0m '+str(p.status_code)
+		if igmeth != 'p':
+			p = requests.post(url_base, timeout=10, headers=headers, 
+								verify=False, allow_redirects=False, 
+								proxies=proxies, data=_POSTdata)
+			_POSTstatus = p.status_code
+			_POSTresponseSize = len(p.content)
+			print '\033[031mPOST: content-length->\033[0m '+str(len(p.content)),
+			print '\033[031m status->\033[0m '+str(p.status_code)
 		
-		if BASE_POSTstatus != BASE_GETstatus:
+		if _POSTstatus != _GETstatus and igmeth != 'p' and igmeth != 'g':
 			print 'POST and GET are different sizes'
 
 	except requests.exceptions.Timeout:
@@ -229,6 +235,8 @@ if __name__ == '__main__':
 						help='Ignore responses of specified size')
 	parse.add_argument('-d', '--data', type=str, default='', 
 						help='Provide default post data (also taken from provided url after ?)')
+	parse.add_argument('-i', '--igmeth', type=str, default='',
+						help='Ignore GET or POST method. Specify g or p')
 	args = parse.parse_args()
 
 	if len(sys.argv) <= 1:
@@ -241,7 +249,7 @@ if __name__ == '__main__':
 	if args.url:
 		version_info()
 		getBase(args.url, args.header, args.agent, args.variance, args.proxy, 
-				args.data)
+				args.data, args.igmeth)
 		print 'Scanning it like you own it...'	
 		try:
 			with open(args.params, "r") as f:
@@ -253,7 +261,8 @@ if __name__ == '__main__':
 		for i in range(0, args.threads):
 			p = multiprocessing.Process(target=requestor, args=(args.url,
 				splitlist[i], args.header, args.agent, args.variance, 
-				args.proxy, args.ignore, args.data, args.out, args.sizeignore))
+				args.proxy, args.ignore, args.data, args.out, args.sizeignore,
+				args.igmeth))
 			threads.append(p)
 		try:
 			for p in threads:
